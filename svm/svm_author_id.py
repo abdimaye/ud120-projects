0f30c81c1a49880b729a5d06e94e38bc8e477270
abdul8002 <abdul8002@gmail.com>
2017-06-12 12:17:57 +0100

    svm method for same training dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#clf = svm.SVC(kernel="linear")
+#clf = svm.SVC(kernel="rbf")
+clf = svm.SVC(kernel="rbf", C=10000)
+
+
+# reduce dataset to 1% to train faster
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)//100]
+
+clf.fit(features_train, labels_train)
+
+predict = clf.predict(features_test)
+
+acc = accuracy_score(predict, labels_test)
+
+print(acc)
+
+# print predictions for elements 10, 26, 50
+# 0 corresponds to Sara and 1 corresoponds to Chris
+print('predictions for elements: 10, 26, 50', predict[10], predict[26], predict[50])
+
+# print all chris 
+# first convert numpy array to a list
+list = list(predict)
+print(list.count(1))
 
 #########################################################
 
